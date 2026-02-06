754b74e1ad6f5605a3a485a5f8b55cb8faec78a9
MarcioAlex-x <marcioalex586@gmail.com>
2026-02-05 22:36:23 -0300

    Adicao de imagem, titulo e bio

--- main.js
+++ main.js
@@ -2,13 +2,19 @@ document.addEventListener('DOMContentLoaded',()=> {
     const titulo = document.querySelector('.titulo')
     const subtitulo = document.querySelector('.subtitulo')
     const bio = document.querySelector('#bio')
+    const imagem = document.querySelector('.imagem')
 
     const fecthData = async () =>{
         const responseUser = await fetch('https://api.github.com/users/MarcioAlex-x')
         const dataUser = await responseUser.json()
+        imagem.src=dataUser.avatar_url
+        imagem.alt=`Imagem de ${dataUser.login}`
+        titulo.innerHTML = dataUser.name
+        bio.innerHTML = dataUser.bio
         
         const fetchRepos = await fetch('https://api.github.com/users/MarcioAlex-x/repos')
         const dataRepos = await fetchRepos.json()
+
     }
 
     fecthData()
